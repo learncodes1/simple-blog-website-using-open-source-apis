207ad5c62f75111f87cd6bc92d1038b80f899461
learncodes1 <learncodes1@gmail.com>
2023-08-27 06:06:20 +0530

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const LoadingList: React.FC = () => {
-  return data.map((item) => (
+  return (
+    <div>
+      {data.map((item) => (
         <div
           key={item}
           className="m-4 max-w-sm rounded-lg border border-gray-200 bg-white shadow"
@@ -35,7 +37,8 @@ const LoadingList: React.FC = () => {
             </div>
           </div>
         </div>
-      )
+      ))}
+    </div>
   );
 };
 export default LoadingList;
