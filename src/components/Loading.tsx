c3f1fcf4369b30456f50290d67defa1a269d2cb4
learncodes1 <learncodes1@gmail.com>
2023-08-27 06:08:09 +0530

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const LoadingList: React.FC = () => {
   return (
-    <div>
+    <>
       {data.map((item) => (
         <div
           key={item}
@@ -38,7 +38,7 @@ const LoadingList: React.FC = () => {
           </div>
         </div>
       ))}
-    </div>
+    </>
   );
 };
 export default LoadingList;
